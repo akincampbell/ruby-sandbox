380e5a8a420cfaa8e4c3702919d2414ca0460156
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-07-24 20:45:38 +0000

    Running Ruby Module

---
+++ howdy.rb
@@ -0,0 +1,9 @@
+#require "active_support/all"
+#my = "Hello, world!"
+#pp my_string.pluralize
+
+#require "./goodbye.rb"
+a = 1
+puts "What's your name?"
+their_name = gets.chomp
+puts "Hello, #{their_name}\!" 
